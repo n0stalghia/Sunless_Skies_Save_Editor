15d5978768998f41a8bcfd032de1f2444703cc55
Mark Ossipow <markosipovs@gmail.com>
2019-02-18 00:21:09 +0100

    Adding layout for the Stats tab

--- __main__.py
+++ __main__.py
@@ -1,7 +1,7 @@
 from PyQt5.QtWidgets import QMainWindow, QApplication, QFileDialog
 from window_design import Ui_MainWindow
 import sys
-import helper
+import window_helper
 
 
 class MainWindow(QMainWindow, Ui_MainWindow):
@@ -12,18 +12,18 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.ui.setupUi(self)
 
         # Button Events
-        self.ui.pushButtonOpen.clicked.connect(self.open_file_dialog)
+        self.ui.pushButton_Open.clicked.connect(self.open_file_dialog)
 
         self.show()
 
     def open_file_dialog(self):
-        file_path = helper.get_file_path(sys.platform)
+        file_path = window_helper.get_file_path(sys.platform)
         options = QFileDialog.Options()
         file_name, _ = QFileDialog.getOpenFileName(self, 'Open', file_path,
                                                    'Autosave (autosave_s.json);;JSON Files (*.json);;All Files (*)',
                                                    options=options)
         if file_name:
-            self.ui.labelLoadedFileName.setText(helper.get_display_name(file_name))
+            self.ui.label_LoadedFileName.setText(window_helper.get_display_name(file_name))
 
 
 if __name__ == '__main__':
